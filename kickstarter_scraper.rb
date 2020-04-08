579e3a1134412bd50b03739a97032eeae67755ad
Bernard  Mel <bernard.mamuyac@gmail.com>
2020-04-08 18:42:19 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,4 +1,6 @@
 # require libraries/modules here
+require "nokogiri"
+require "pry"
 
 def create_project_hash
   # write your code here
@@ -6,5 +8,7 @@ def create_project_hash
   html = File.read('fixtures/kickstarter.html')
 
   kickstarter = Nokogiri::HTML(html)
-  
+  binding.pry
 end
+
+create_project_hash
